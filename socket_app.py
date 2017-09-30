e1cb01fd7f6865296cfdfa24a510af74131e1fa5
Benjamin Braatz <sean@heptasean.de>
2017-09-30 14:23:18 +0000

    Design Web app
    * CSS for buttons and status message
    * Query string removed by Javascript

--- socket_app.py
+++ socket_app.py
@@ -12,21 +12,61 @@ def application(environ, start_response):
     elif socket == 'D':
         send_code(1300)
     else:
-        socket == ''
+        socket = ''
     body = """<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Socket Switching App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+body {
+    margin: 0;
+    font-family: sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+}
+a.button {
+    display: block;
+    box-sizing: border-box;
+    width: 80%;
+    padding: 6px;
+    margin: 10px 10%;
+    border-radius: 10px;
+    border-style: solid;
+    border-width: 4px;
+    border-color: #aaaaff;
+    background-color: #aaaaff;
+    color: #000000;
+    text-decoration: none;
+    text-align: center;
+}
+a.button:hover {
+    border-color: #0000ff;
+}
+a.button:visited {
+}
+p.status {
+    box-sizing: border-box;
+    width: 80%;
+    padding: 10px;
+    margin: 10px 10%;
+    background-color: #aaffaa;
+}
+    </style>
+    <script>
+history.replaceState(null, "Socket Switching App", location.href.split('?')[0]);
+    </script>
   </head>
-  <body>"""
+  <body>
+    <a class="button" href="?a">Switch Socket A</a>
+    <a class="button" href="?b">Switch Socket B</a>
+    <a class="button" href="?c">Switch Socket C</a>
+    <a class="button" href="?d">Switch Socket D</a>"""
     if socket:
-        body += "\n<p>Socket {} switched</p>".format(socket)
+        body += """
+    <p class="status">Socket {} switched.</p>""".format(socket)
     body += """
-    <a href="?a">Switch Button A</a>
-    <a href="?b">Switch Button B</a>
-    <a href="?c">Switch Button C</a>
-    <a href="?d">Switch Button D</a>
   </body>
 </html>"""
     body = body.encode('utf-8')
