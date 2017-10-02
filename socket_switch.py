349fe080e4233f7bfd57576265d9702f3aa11133
Benjamin Braatz <sean@heptasean.de>
2017-10-02 22:14:43 +0000

    Switch from wiringpi to RPi.GPIO

--- socket_switch.py
+++ socket_switch.py
@@ -1,36 +1,37 @@
 import sys
-import wiringpi
+import time
+from RPi import GPIO
 
-PIN = 0
+PIN = 17
 PULSE_LENGTH = 350
 BITS = 24
 REPEAT = 10
+CODES = {'A': 340, 'B': 1108, 'C': 1348, 'D': 1300}
 
-wiringpi.wiringPiSetup()
-wiringpi.pinMode(PIN, 1)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setwarnings(False)
 
 def send_code(code):
     bits = format(code, '0{}b'.format(BITS))
     for _ in range(REPEAT):
+        GPIO.output(PIN, GPIO.HIGH)
+        time.sleep(   PULSE_LENGTH/1000000.0)
+        GPIO.output(PIN, GPIO.LOW)
+        time.sleep(31*PULSE_LENGTH/1000000.0)
         for bit in bits:
             if bit == '0':
-                wiringpi.digitalWrite(PIN, 1)
-                wiringpi.delayMicroseconds(  PULSE_LENGTH)
-                wiringpi.digitalWrite(PIN, 0)
-                wiringpi.delayMicroseconds(3*PULSE_LENGTH)
+                GPIO.output(PIN, GPIO.HIGH)
+                time.sleep(  PULSE_LENGTH/1000000.0)
+                GPIO.output(PIN, GPIO.LOW)
+                time.sleep(3*PULSE_LENGTH/1000000.0)
             if bit == '1':
-                wiringpi.digitalWrite(PIN, 1)
-                wiringpi.delayMicroseconds(3*PULSE_LENGTH)
-                wiringpi.digitalWrite(PIN, 0)
-                wiringpi.delayMicroseconds(  PULSE_LENGTH)
-        wiringpi.digitalWrite(PIN, 1)
-        wiringpi.delayMicroseconds(   PULSE_LENGTH)
-        wiringpi.digitalWrite(PIN, 0)
-        wiringpi.delayMicroseconds(31*PULSE_LENGTH)
+                GPIO.output(PIN, GPIO.HIGH)
+                time.sleep(3*PULSE_LENGTH/1000000.0)
+                GPIO.output(PIN, GPIO.LOW)
+                time.sleep(  PULSE_LENGTH/1000000.0)
 
 if __name__ == '__main__':
-    codes = {'A': 340, 'B': 1108, 'C': 1348, 'D': 1300}
-
     argc = len(sys.argv) - 1
     if argc < 1 or argc > 1:
         print("Expected exactly one argument (the socket), " +
@@ -38,10 +39,11 @@ if __name__ == '__main__':
         exit(1)
 
     socket = sys.argv[1].upper()
-    if not socket in codes:
+    if not socket in CODES:
         print("Socket {} not known!".format(socket))
         exit(1)
 
-    code = codes[socket]
+    code = CODES[socket]
     print("Sending code '{}'.".format(code))
     send_code(code)
+    GPIO.cleanup()
